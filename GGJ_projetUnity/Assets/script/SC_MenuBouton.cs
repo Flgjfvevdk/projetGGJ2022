73ea51acab73611a7077bf21d44baace0c389597
satriton <80455175+satriton@users.noreply.github.com>
2022-01-27 05:44:19 +0100

    Création menu

---
+++ GGJ_projetUnity/Assets/script/SC_MenuBouton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_MenuBouton : MonoBehaviour
+{
+    public void MenuToGame()
+    {
+        Debug.Log("Redirection vers le jeu");
+        SceneManager.LoadScene("SampleScene");
+    }
+}
