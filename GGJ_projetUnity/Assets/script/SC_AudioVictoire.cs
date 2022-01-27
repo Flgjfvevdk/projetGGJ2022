4188eaa0bfa582749d08a00d48bc328903f3e1df
satriton <80455175+satriton@users.noreply.github.com>
2022-01-28 00:45:40 +0100

    ajout de son

---
+++ GGJ_projetUnity/Assets/script/SC_AudioVictoire.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SC_AudioVictoire : MonoBehaviour
+{
+    public float volume;
+    public float pitch;
+
+    private AudioSource[] source;
+    
+    void Awake() 
+    {
+        source = GetComponents<AudioSource>();
+    }
+
+    void Start()
+    {
+        lancerAudio();
+    }
+
+    public void lancerAudio()
+    {
+        for(int i = 0;i<source.Length;i++) {
+            source[i].Play();
+        }
+        
+        Debug.Log("jouer le son de la fete");
+    }
+}
