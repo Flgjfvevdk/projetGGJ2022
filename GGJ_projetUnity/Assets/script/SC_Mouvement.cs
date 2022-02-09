3883172b7c622b09339dc8547ff42b351f7160b2
Flgjfvevdk <74116706+Flgjfvevdk@users.noreply.github.com>
2022-02-09 01:01:02 +0100

    ékilibrage

--- GGJ_projetUnity/Assets/script/SC_Mouvement.cs
+++ GGJ_projetUnity/Assets/script/SC_Mouvement.cs
@@ -142,7 +142,6 @@ public class SC_Mouvement : MonoBehaviour
         //Passer à travers plateforme
         if (downPressed || rb.velocity.y > 0.01f)
         {
-            Debug.Log(rb.velocity.y);
             Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Player"), LayerMask.NameToLayer("Plateforme"), true);
         }
         else
